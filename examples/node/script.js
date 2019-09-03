d5cd411c2621f617875b7c98bb835682b0a7307d
Leonardo Cabeza <cabeza.leo@gmail.com>
2019-09-02 21:29:18 -0500

    Updated node example to match repl.it

--- examples/node/script.js
+++ examples/node/script.js
@@ -1,6 +1,11 @@
 const { v3, v4 } = require('@leonardocabeza/the-movie-db');
 
+// You can read the FAQ on how to get a token at:
+// https://www.themoviedb.org/faq/api?language=en-US 
+
+// For v3 endpoints (https://developers.themoviedb.org/3):
 const v3ApiKey = 'YOUR_V3_API_KEY';
+// For v4 endpoints (https://developers.themoviedb.org/4):
 const v4ApiKey = 'YOUR_V4_API_KEY';
 const userToken = 'AN_USER_ACCESS_TOKEN';
 const accountId = 'AN_ACCOUNT_ID';
